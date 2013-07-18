a2ac78e36622d81fb9cb451847ac8a9dee043097
dnahodil <dnahodil@utas.edu.au>
2013-07-18 13:52:33 +1000

    Initial version of GeoNetwork checker

---
+++ link_checker/lib/get_links_from_metadata.groovy
@@ -0,0 +1,107 @@
+import groovy.sql.Sql
+
+@Grapes([
+	@Grab('postgresql:postgresql:9.0-801.jdbc3'),
+	@GrabConfig(systemClassLoader=true)
+])
+
+// Variables
+FAILURE = -1
+SUCCESS = 0
+
+urlPattern = ~/https?:\\/\\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:\/~\+#]*[\w\-\@?^=%&\/~\+#])?/
+recordCount = 0
+foundUrls = [] as Set
+
+// Load script arguments
+if (argsValid()) {
+
+	loadArguments()
+}
+else {
+
+	printUsage()
+	System.exit(FAILURE)
+}
+
+// Do the query
+try {
+	log "Connecting to: $connectionUrl"
+
+	def sql = Sql.newInstance(
+		connectionUrl,
+		username,
+		password,
+		"org.postgresql.Driver"
+	)
+
+	sql.eachRow("SELECT id, data FROM metadata") {
+
+		extractUrls(it, urlPattern)
+	}
+}
+catch(Exception e) {
+
+	writeErr "Unable to execute query on $connectionUrl"
+	e.printStackTrace(System.err)
+	System.exit(FAILURE)
+}
+
+// Write output
+foundUrls.each{ write it }
+
+log "$recordCount records searched"
+log "${foundUrls.size()} unique URLs found"
+
+System.exit(SUCCESS)
+
+// Supporting logic
+
+void loadArguments() {
+
+	def safeArgs = args.toList() << "" // Ensure a 5th element for safe access
+
+	// Load args from command line
+	(host, database, username, password, loggingEnabled) = safeArgs
+
+	connectionUrl = "jdbc:postgresql://$host:5432/$database?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+}
+
+def argsValid() {
+
+	return args.length == 4 ||
+           args.length == 5 && args.last() == "-v"
+}
+
+void printUsage() {
+
+	writeErr "Please provide the database connection details. -v adds logging."
+	writeErr "Usage: groovy .\\get_links_from_metadata.groovy <host> <database_name> <username> <password> [-v]"
+}
+
+void extractUrls(record, pattern) {
+
+	def matcher = pattern.matcher(record.data)
+
+	def matchedUrls = matcher.collect{it.first()} // First result is whole URL
+
+	log "Record ${record.id} had ${matchedUrls.size()} matched URLs"
+
+	recordCount++
+	foundUrls.addAll matchedUrls
+}
+
+void write(s) {
+
+	println s
+}
+
+void writeErr(s) {
+
+	System.err.println s
+}
+
+void log(s) {
+
+	if (loggingEnabled) writeErr s
+}
