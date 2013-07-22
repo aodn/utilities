e6ccc902c526101ce1654d6a28160fa7ace92340
dnahodil <dnahodil@utas.edu.au>
2013-07-22 14:50:44 +1000

    Code review changes

--- link_checker/lib/cat_geonetwork_metadata_links.groovy
+++ link_checker/lib/cat_geonetwork_metadata_links.groovy
@@ -1,71 +1,80 @@
-import groovy.sql.Sql
-
 @Grapes([
 	@Grab('postgresql:postgresql:9.0-801.jdbc3'),
 	@GrabConfig(systemClassLoader=true)
 ])
 
+import groovy.sql.Sql
+
 // Exit codes
-FAILURE = -1
+FAILURE = 1
 SUCCESS = 0
 
-// Load script arguments
-if (!argsValid()) {
+execute()
 
-	printUsage()
-	System.exit(FAILURE)
-}
+void execute() {
 
-loadArguments()
-initVariables()
+	// Load script arguments
+	if (!argsValid()) {
 
-// Do the query
-try {
-	log "Connecting to: $connectionUrl"
-
-	def sql = Sql.newInstance(
-		connectionUrl,
-		username,
-		password,
-		"org.postgresql.Driver"
-	)
-
-	debug "Connection successful. Executing query."
-
-	sql.eachRow("SELECT id, data FROM metadata") {
-
-		extractUrls(it, urlPattern)
+		printUsage()
+		System.exit(FAILURE)
 	}
+
+	loadArguments()
+	initVariables()
+
+	// Do the query
+	try {
+		log "Connecting to: $connectionUrl"
+
+		def sql = Sql.newInstance(
+			connectionUrl,
+			username,
+			password,
+			"org.postgresql.Driver"
+		)
+
+		debug "Connection successful. Executing query."
+
+		sql.eachRow("SELECT id, data FROM metadata") {
+
+			extractUrls(it, urlPattern)
+		}
+	}
+	catch(Exception e) {
+
+		writeErr "Unable to execute query on $connectionUrl"
+		e.printStackTrace(System.err)
+		System.exit(FAILURE)
+	}
+
+	// Write output
+	foundUrls.each{ write it }
+
+	log "$recordCount records searched"
+	log "${foundUrls.size()} unique URLs found"
+
+	System.exit(SUCCESS)
 }
-catch(Exception e) {
-
-	writeErr "Unable to execute query on $connectionUrl"
-	e.printStackTrace(System.err)
-	System.exit(FAILURE)
-}
-
-// Write output
-foundUrls.each{ write it }
-
-log "$recordCount records searched"
-log "${foundUrls.size()} unique URLs found"
-
-System.exit(SUCCESS)
-
-// Supporting logic
 
 def argsValid() {
 
-	return args.length == 4 ||
-           args.length == 5 && args.last() == "-v"
+	return args.length == 5 ||
+           args.length == 6 && args.last() == "-v"
+}
+
+void printUsage() {
+
+	writeErr "Please provide the database connection details. -v adds debug logging."
+	writeErr "Usage: groovy .\\cat_geonetwork_metadata_links.groovy <host> <port> <database_name> <username> <password> [-v]"
 }
 
 void loadArguments() {
 
-	def safeArgs = args.toList() << "" // Ensure a 5th element for safe access
+	def safeArgs = args.toList() << "" // Ensure a 6th element for safe access
 
 	// Load args from command line
-	(host, database, username, password, debuggingEnabled) = safeArgs	
+	(host, port, database, username, password, debuggingEnabled) = safeArgs	
 }
 
 void initVariables() {
@@ -73,13 +82,7 @@ void initVariables() {
 	urlPattern = ~/https?:\\/\\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:\/~\+#]*[\w\-\@?^=%&\/~\+#])?/
 	recordCount = 0
 	foundUrls = [] as Set
-	connectionUrl = "jdbc:postgresql://$host:5432/$database?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
-}
-
-void printUsage() {
-
-	writeErr "Please provide the database connection details. -v adds debug logging."
-	writeErr "Usage: groovy .\\cat_geonetwork_metadata_links.groovy <host> <database_name> <username> <password> [-v]"
+	connectionUrl = "jdbc:postgresql://$host:$port/$database?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
 }
 
 void extractUrls(record, pattern) {
