008a89814b53133750ac6d68816681cf32c1fd0a
pmbohm <pmbohm@utas.edu.au>
2017-11-02 08:52:35 +1100

    Cloudformation AutoOff Beanstalk Lambda

---
+++ cloudformation/lambda/AWS-Cleanup/AutoOffBeanstalk.py
@@ -0,0 +1,46 @@
+import logging
+import boto3
+
+# setup simple logging for INFO
+
+# define the connection
+client = boto3.client('elasticbeanstalk')
+skip_envs = ["geoserver-systest", "geowebcache-systest", 'aodn-portal-sandbox', 'geoserver-sandbox', 'geowebcache-sandbox', 'thredds-sandbox']
+
+def handler(event, context):
+    response_de = client.describe_environments(IncludeDeleted=False)
+    logger.debug('Beanstalk Environments: ' + str(response_de))
+
+    for env in response_de['Environments']:
+
+        if not env['EnvironmentName'].endswith('-prod') \
+                and env['EnvironmentName'] not in skip_envs \
+                and env['Status'] in ('Updating','Ready'):
+
+            logger.info("TERMINATING {0} ({1})".format(env['EnvironmentName'], env['Status']))
+            ###############
+            # terminate(env)
+            ####################
+        else:
+            logger.info("EXCLUDING {0} ({1})".format(env['EnvironmentName'], env['Status']))
+
+
+def terminate(env):
+    terminating_env = client.terminate_environment(
+        EnvironmentId=env['EnvironmentId'],
+        EnvironmentName=env['EnvironmentName'],
+        TerminateResources=True,
+        ForceTerminate=True
+    )
+    logger.debug('ApplicationName: ' + terminating_env['ApplicationName'])
+    logger.debug('EnvironmentName: ' + terminating_env['EnvironmentName'])
+    logger.debug('EnvironmentId: ' + terminating_env['EnvironmentId'])
+    logger.debug('Status: ' + terminating_env['Status'])
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    handler(None,None)
+
+
