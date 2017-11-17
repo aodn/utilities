a45df378cc22768735eae216c84073e8c76d733d
Alex McKeown <a.mckeown@utas.edu.au>
2017-11-17 13:42:33 +1100

    Cleaning up check_access_keys Rename root folder for correct capitalisation. Move code to root folder to make things easier for the jenkins job we're going to create. Split "click" code into it's own class. Add a line of logging.

--- cloudformation/lambda/Check_access_keys/check_access_keys.py
+++ cloudformation/lambda/Check_access_keys/check_access_keys.py
@@ -1,7 +1,6 @@
 import logging
 import datetime
 import boto3
-import click
 
 # setup simple logging for INFO
 logging.basicConfig()
@@ -12,15 +11,9 @@ logger.setLevel(logging.INFO)
 client = boto3.client('iam')
 
 
-@click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-e', '--excluded_users', type=click.STRING, envvar='excluded_users', multiple=True,
-              help='Users whose access keys should not be checked')
-@click.option('-t', '--topic', type=click.STRING, envvar='topic', required=True,
-              help='The AWS arn of the SNS topic where the notification will be published')
-@click.option('-a', '--age_threshold', type=click.INT, envvar='age_threshold', default=90,
-              help='Age threshold in days. Any access key older than the age threshold will trigger a notification')
-def main(excluded_users, topic, age_threshold):
-    execute(excluded_users, topic, age_threshold)
+def lambda_handler(event, context):
+    age_threshold = int(event['age_threshold'])
+    execute(event['excluded_users'], event['topic'], age_threshold)
 
 
 def execute(excluded_users, topic, age_threshold):
@@ -46,16 +39,9 @@ def execute(excluded_users, topic, age_threshold):
                 message += 'Age: ' + str(age.days) + ' days' + '\n'
 
     if hit:
+        logger.info(message)
         sns_client = boto3.client('sns')
         sns_client.publish(
             TopicArn=topic,
             Message=message,
             Subject='Access Key Rotation Reminder')
-
-
-def lambda_handler(event, context):
-    age_threshold = int(event['age_threshold'])
-    execute(event['excluded_users'], event['topic'], age_threshold)
-
-if __name__ == '__main__':
-    main()
