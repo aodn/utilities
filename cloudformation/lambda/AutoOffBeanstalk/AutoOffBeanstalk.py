c19930bf5297c4b2fd6fb437bf5839be27649a96
Alex McKeown <a.mckeown@utas.edu.au>
2017-11-27 14:10:19 +1100

    Un-inline the code. Requires the use of cloudformation package command to create a deployable yaml.

--- cloudformation/lambda/AutoOffBeanstalk/AutoOffBeanstalk.py
+++ cloudformation/lambda/AutoOffBeanstalk/AutoOffBeanstalk.py
@@ -2,10 +2,14 @@ import logging
 import boto3
 
 # setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # define the connection
 client = boto3.client('elasticbeanstalk')
-skip_envs = ["geoserver-systest", "geowebcache-systest", 'aodn-portal-sandbox', 'geoserver-sandbox', 'geowebcache-sandbox', 'thredds-sandbox']
+skip_envs = ["geoserver-systest", "geowebcache-systest", 'aodn-portal-sandbox', 'geoserver-sandbox',
+             'geowebcache-sandbox', 'thredds-sandbox']
+
 
 def handler(event, context):
     response_de = client.describe_environments(IncludeDeleted=False)
@@ -15,12 +19,12 @@ def handler(event, context):
 
         if not env['EnvironmentName'].endswith('-prod') \
                 and env['EnvironmentName'] not in skip_envs \
-                and env['Status'] in ('Updating','Ready'):
+                and env['Status'] in ('Updating', 'Ready'):
 
             logger.info("TERMINATING {0} ({1})".format(env['EnvironmentName'], env['Status']))
             ###############
-            # terminate(env)
-            ####################
+            terminate(env)
+            ###############
         else:
             logger.info("EXCLUDING {0} ({1})".format(env['EnvironmentName'], env['Status']))
 
@@ -36,6 +40,7 @@ def terminate(env):
     logger.debug('EnvironmentName: ' + terminating_env['EnvironmentName'])
     logger.debug('EnvironmentId: ' + terminating_env['EnvironmentId'])
     logger.debug('Status: ' + terminating_env['Status'])
+
 
 if __name__ == "__main__":
     logging.basicConfig()
