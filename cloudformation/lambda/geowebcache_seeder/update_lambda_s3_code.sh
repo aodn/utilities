59c0eaab986aced9a4d57c4794bc5fa6e6e797c9
Alex McKeown <a.mckeown@utas.edu.au>
2018-05-24 13:57:25 +1000

    Remove all lambdas The lambdas we still use have been moved to the cloud-deploy repo

--- cloudformation/lambda/geowebcache_seeder/update_lambda_s3_code.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-aws lambda update-function-code --function-name geowebcache_seed --s3-bucket imos-binary --s3-key lambda/geowebcache_seeder.zip
-aws lambda update-function-code --function-name geowebcache_purge --s3-bucket imos-binary --s3-key lambda/geowebcache_seeder.zip
-aws lambda update-function-code --function-name geowebcache_truncate --s3-bucket imos-binary --s3-key lambda/geowebcache_seeder.zip
