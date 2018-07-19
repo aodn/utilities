afc0a5eb073d9cf5a9ff1bb8d29e61388d0ff88c
gsatimos <george.sattler@utas.edu.au>
2018-07-19 13:08:38 +1000

    fix issue when gn response is unicode

--- geonetwork/gn-tool/get-uuids.py
+++ geonetwork/gn-tool/get-uuids.py
@@ -1,9 +1,11 @@
 from xml.etree.ElementTree import fromstring, ElementTree
 import sys
+import unicodedata
 
 import requests
 
 text_xml = requests.get(sys.argv[1]).text
+text_xml = text_xml.encode('ascii','ignore')
 xml_tree = ElementTree(fromstring(text_xml))
 
 uuids = {uuid.text for uuid in xml_tree.findall('.//uuid')}
