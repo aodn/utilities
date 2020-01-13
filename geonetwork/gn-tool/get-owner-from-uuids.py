c3c5cada556e8bdb663c2308c6172a2f295bd12e
Sachit Rajbhandari <sachit.rajbhandari@utas.edu.au>
2020-01-13 16:36:46 +1100

    Assign records to the same groups/users during migration from GN2 to GN3

---
+++ geonetwork/gn-tool/get-owner-from-uuids.py
@@ -0,0 +1,36 @@
+# Retrieves owner of the specified metadata record
+
+from xml.etree.ElementTree import fromstring, ElementTree
+import argparse
+import sys
+from requests.auth import HTTPBasicAuth
+import xml
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url')
+parser.add_argument('--username', default=None)
+parser.add_argument('--password', default=None)
+parser.add_argument('--xsrftoken', default=None)
+args = parser.parse_args()
+
+headers = {}
+if args.username and args.password:
+    auth = HTTPBasicAuth(args.username, args.password)
+    if args.xsrftoken:
+        headers["X-XSRF-TOKEN"] = args.xsrftoken
+else:
+    auth = None
+
+text_xml = requests.get(args.url, headers=headers, auth=auth).text
+text_xml = text_xml.encode('ascii', 'ignore')
+
+try:
+    xml_tree = ElementTree(fromstring(text_xml))
+except xml.etree.ElementTree.ParseError as e:
+    print("Couldn't parse XML response: " + str(text_xml))
+    sys.exit(1)
+
+owner = xml_tree.find('.//metadata/owner').text
+
+print(owner)
