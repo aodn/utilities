5605a166cd44ace52ea7bdf4febc99617773cbf4
Craig Jones <Craig.Jones@utas.edu.au>
2020-03-06 18:20:52 +1100

    Rename transformCatalogue to follow java naming conventions for classes. Add example unit test for testing data migration

---
+++ geonetwork/transform-mcp-iso19115-3/src/test/java/au/org/aodn/xslt/TransformCatalogueTest.java
@@ -0,0 +1,81 @@
+package au.org.aodn.xslt;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.junit.Assert.assertEquals;
+
+public class TransformCatalogueTest {
+
+    private Path testDir;
+    private Path resourceDir = Paths.get("src/test/resources");
+
+    @Before
+    public void setup() throws IOException {
+        // Create test directory
+        testDir = Files.createTempDirectory("export-");
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        // Delete test directory
+        deleteFolder(testDir);
+    }
+
+    @Test
+    public void runTest() throws IOException {
+        // Copy test files to test directory
+        copyFolder(resourceDir.resolve("export"), testDir);
+
+        // Run transform
+        String args[] = {
+            "TransformCatalogue",
+            "-d", testDir.toString(),
+            "-i", "metadata.xml",
+            "-o", "metadata.iso19115-3.2018.xml",
+            "-g", "http://catalogue-imos.dev.aodn.org.au/geonetwork",
+            "-u"
+        };
+        TransformCatalogue.main(args);
+
+        // Assert each test result matches expected result
+        for (File testCaseDir: testDir.toFile().listFiles(File::isDirectory)) {
+            String testCaseName = testCaseDir.getName();
+            Path expectedFile = resourceDir.resolve("expected").resolve(testCaseName + ".xml");
+            Path actualFile = testCaseDir.toPath().resolve("metadata/metadata.iso19115-3.2018.xml");
+            String expectedResult = new String(Files.readAllBytes(expectedFile));
+            String actualResult = new String(Files.readAllBytes(actualFile));
+            assertEquals(expectedResult, actualResult);
+            //assertThat(actualResult, CompareMatcher.isIdenticalTo(expectedResult).ignoreWhitespace());
+        }
+    }
+
+    private void deleteFolder(Path pathToBeDeleted) throws IOException {
+        Files.walk(pathToBeDeleted)
+            .sorted(Comparator.reverseOrder())
+            .map(Path::toFile)
+            .forEach(File::delete);
+    }
+
+    private void copyFolder(Path src, Path dest) throws IOException {
+        Files.walk(src)
+            .forEach(source -> copy(source, dest.resolve(src.relativize(source))));
+    }
+
+    private void copy(Path source, Path dest) {
+        try {
+            Files.copy(source, dest, REPLACE_EXISTING);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
