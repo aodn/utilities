86e4f3519314da204686d35815e01881b3720d06
pmbohm <pmbohm@utas.edu.au>
2020-04-03 13:01:56 +1100

    gn-tools harvested records option

--- geonetwork/gn-tool/gn3-tool.sh
+++ geonetwork/gn-tool/gn3-tool.sh
@@ -41,11 +41,13 @@ get_deleted_records() {
 # $2 - geonetwork user
 # $3 - geonetwork password
 # $4 - geonetwork XSRF token
+# $5 - download harvested records
 get_all_records() {
     local gn_addr=$1; shift
     local gn_user=$1; shift
     local gn_password=$1; shift
     local gn_xsrftoken=$1; shift
+    local harvest_all=$1; shift
 
     if [[ $gn_user != "" ]]; then
       gn_user_arg="--username $gn_user"
@@ -57,7 +59,13 @@ get_all_records() {
       gn_xsrftoken_arg="--xsrftoken $gn_xsrftoken"
     fi
 
-    python ./get-uuids.py "$gn_addr/srv/eng/xml.search?fast=index&_isHarvested=n&from=1&to=2000" $gn_user_arg $gn_password_arg $gn_xsrftoken_arg
+    if [[ $harvest_all != "" ]]; then
+      harvest_all_arg="y"
+    else
+      harvest_all_arg="n"
+    fi
+
+    python ./get-uuids.py "$gn_addr/srv/eng/xml.search?fast=index&_isHarvested=$harvest_all_arg&from=1&to=2000" $gn_user_arg $gn_password_arg $gn_xsrftoken_arg
 
     # NOTE:
     # Returns total no of records based on the value of 'maxRecords' in config-service-search.xml for Geonetwork 3.
@@ -296,6 +304,7 @@ export_records() {
     local gn_addr=$1; shift
     local gn_user=$1; shift
     local gn_password=$1; shift
+    local harvest_all=$1; shift
 
     users=$(get_users_gn2 $gn_addr $gn_user $gn_password)
 
@@ -309,7 +318,7 @@ export_records() {
     if [ x"$record_uuid" = x"ALL" ]; then
         mkdir -p $record_dir
 
-        text_result=$(get_all_records $gn_addr $gn_user $gn_password $XSRFTOKEN)
+        text_result=$(get_all_records $gn_addr $gn_user $gn_password $XSRFTOKEN $harvest_all)
 
         if [[ $? -ne "0" ]]; then
           echo "Error in finding the uuids"
@@ -557,14 +566,15 @@ Options:
   -u                         Username to login with.
   -p                         Password to login with.
   -y                         Import action type must be one of 'overwrite', or 'nothing' or 'generateUUID'
-  -z                         Genetwork group id."
+  -z                         Genetwork group id.
+  -a                         export all harvested records 'yes'"
     exit 3
 }
 
 main() {
     # parse options with getopt
     local tmp_getops
-    tmp_getops=`getopt hGo:l:r:t:g:u:p:y:z: $*`
+    tmp_getops=`getopt hGo:l:r:t:g:u:p:y:z:a: $*`
     [ $? != 0 ] && usage
 
     set -- $tmp_getops
@@ -575,6 +585,7 @@ main() {
     local group=2
     local uuid_action="nothing"
     local uuid_tag="uuid"
+    local harvest_all
 
     # parse the options
     while true ; do
@@ -590,6 +601,7 @@ main() {
             -p) gn_password="$2"; shift 2;;
             -y) uuid_action="$2"; shift 2;;
             -z) group="$2"; shift 2;;
+            -a) harvest_all="$2"; shift 2;;
             --) shift; break;;
             *) usage;;
         esac
@@ -619,7 +631,7 @@ main() {
             import_records $location $gn_addr $gn_user $gn_password $group $uuid_action
         fi
     elif [ "$operation" = "export" ]; then
-        export_records $record_uuid $uuid_tag $location $gn_addr $gn_user $gn_password
+        export_records $record_uuid $uuid_tag $location $gn_addr $gn_user $gn_password $harvest_all
     else
         usage
     fi
